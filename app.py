1d6a41c695f3b5512e1562dc363e943e8b6f0825
seong10 <kck555511@naver.com>
2022-05-26 16:59:29 +0900

    add

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from app_ml import run_ml
 def main() :
     st.title('미래의 미세먼지 예측')
 
-    menu = ['Home', 'EDA', 'ML']
+    menu = ['Home', '데이터', '미래의 미세먼지 예측']
     choice = st.sidebar.selectbox('메뉴를 선택하세요', menu)
 
     if choice == menu[0] :
